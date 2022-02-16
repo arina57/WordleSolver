62f070f1436ddb03472face8e8551f4e2bf762d8
Arina Anjong <arina_57@hotmail.co.jp>
2022-02-16 23:17:01 +0900

    Added ranking by letter position

--- WordleSovlerLibrary/WordAnalyzer.cs
+++ WordleSovlerLibrary/WordAnalyzer.cs
@@ -13,11 +13,11 @@ namespace WordleSovlerLibrary
         private List<char>[] knownNotUsedPositions = Enumerable.Range(1, 5).Select(x => new List<char>()).ToArray();
         private HashSet<char> knownLetters = new HashSet<char>();
 
+        private char[][] letterPositionRanking;
         private static string FilePath => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Resources\words_alpha.txt");
         private IEnumerable<string> validWords = File.ReadAllLines(FilePath).Where(word => word.Length == 5).Select(word => word.ToLower());
         public Dictionary<char,int> LetterWordCount(IEnumerable<string> wordList) => letters.Select(letter => (letter, wordList.Sum(word => word.Contains(letter) ? 1 : 0))).ToDictionary(item => item.letter, item => item.Item2);
         
-
 
         public void AddGuess(string guess, List<Enums.Result> result)
         {
@@ -46,22 +46,43 @@ namespace WordleSovlerLibrary
         }
 
 
+        public Dictionary<char, int>[] LetterPositionRank(IEnumerable<string> words)
+        {
+            var letterRanks = Enumerable.Range(1, 5).Select(x => letters.ToDictionary(letter => letter, letter => 0)).ToArray();
+            foreach (var word in words)
+            {
+                for (var i = 0; i < word.Length; i++)
+                {
+                    letterRanks[i][word[i]]++;
+                }
+            }    
+            return letterRanks;
+        }
+
         public IEnumerable<string> FindValidWords(IEnumerable<string> wordList) => 
             wordList.Where((word) => !word.Where((letter, i) =>
                             knownNotUsedPositions[i].Contains(word[i]) || (knownPositionLetters[i] != null && knownPositionLetters[i] != word[i])).Any()
                             && !word.Any(letter => notUsedLetters.Contains(letter))
                             && knownLetters.All(letter => knownLetters.Count == 0 || word.Contains(letter)));
 
-        public Dictionary<string, int> RankWords(IEnumerable<string> wordsToRank)
+
+        public Dictionary<string, int> RankWordByLetterCount(IEnumerable<string> wordsToRank)
         {
             var letterCount = LetterWordCount(wordsToRank);
             var wordScore = wordsToRank.ToDictionary(word => word, word => word.Sum(letter => word.Count(l => letter == l) == 1 ? letterCount[letter] : 0));
             return wordScore;
         }
+
+        public Dictionary<string, int> RankWordsByLetterPosition(IEnumerable<string> wordsToRank)
+        {
+            var positionRank = LetterPositionRank(wordsToRank);
+            var wordPosScore = wordsToRank.ToDictionary(word => word, word => word.Select((letter, index) => (letter, index)).Sum(item => positionRank[item.index][item.letter]));
+            return wordPosScore;
+        }
         public List<string> SuggestGuess()
         {
             validWords = FindValidWords(validWords);
-            return RankWords(validWords).OrderByDescending(score => score.Value).Select(word => word.Key).Take(10).ToList(); 
+            return RankWordsByLetterPosition(validWords).OrderByDescending(score => score.Value).Select(word => word.Key).Take(10).ToList(); 
         }
       }
 }
