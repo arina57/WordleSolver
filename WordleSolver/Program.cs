177f2b27823c3a7c5fed8c8aca793c1106cc79c2
Arina Anjong <arina_57@hotmail.co.jp>
2022-02-16 21:51:12 +0900

    Changed word count

--- WordleSolver/Program.cs
+++ WordleSolver/Program.cs
@@ -6,9 +6,9 @@ Console.WriteLine("Hello, World!");
 
 WordleSovlerLibrary.WordAnalyzer wordAnalyzer = new WordleSovlerLibrary.WordAnalyzer();
 Console.WriteLine(wordAnalyzer.SuggestGuess());
-wordAnalyzer.AddGuess("arose", new List<Result> { Result.Yellow, Result.Grey, Result.Grey, Result.Grey, Result.Grey });
+wordAnalyzer.AddGuess("aesir", new List<Result> { Result.Yellow, Result.Grey, Result.Grey, Result.Grey, Result.Grey });
 Console.WriteLine(wordAnalyzer.SuggestGuess());
-wordAnalyzer.AddGuess("inula", new List<Result> { Result.Grey, Result.Grey, Result.Green, Result.Green, Result.Yellow });
+wordAnalyzer.AddGuess("notal", new List<Result> { Result.Grey, Result.Grey, Result.Grey, Result.Yellow, Result.Yellow });
 Console.WriteLine(wordAnalyzer.SuggestGuess());
 wordAnalyzer.AddGuess("dault", new List<Result> { Result.Grey, Result.Green, Result.Green, Result.Green, Result.Grey });
 Console.WriteLine(wordAnalyzer.SuggestGuess());
--- WordleSovlerLibrary/WordAnalyzer.cs
+++ WordleSovlerLibrary/WordAnalyzer.cs
@@ -8,7 +8,6 @@ namespace WordleSovlerLibrary
     {
 
         private char[] letters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
-        private List<char> letterRanking = new List<char>();
         private HashSet<char> notUsedLetters = new HashSet<char>();
         private char?[] knownPositionLetters = new char?[5];
         private List<char>[] knownNotUsedPositions = Enumerable.Range(1, 5).Select(x => new List<char>()).ToArray();
@@ -17,24 +16,20 @@ namespace WordleSovlerLibrary
         private static string FilePath => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Resources\words_alpha.txt");
         private List<string> wordList = File.ReadAllLines(FilePath).Where(word => word.Length == 5).Select(word => word.ToLower()).ToList();
 
-        private Dictionary<char,int> letterCount = new Dictionary<char,int>();
 
-        public void RankLetters()
+        public Dictionary<char,int> LetterWordCount()
         {
-            letterCount = letters.ToDictionary(item => item, item => 0);
+            var letterCount = letters.ToDictionary(item => item, item => 0);
             
             foreach(var word in wordList)
             {
-                     foreach(var key in letterCount.Keys)
+                    foreach(var key in letterCount.Keys)
                     {
-                    letterCount[key] += word.Count(word => word == key);
+                        letterCount[key] += word.Contains(key) ? 1 : 0;
                     }
 
             }
-
-            var letterRank = letterCount.OrderByDescending(keyPair => keyPair.Value);
-            letterRanking = letterRank.Select(keyPair => keyPair.Key).ToList();
-            
+            return letterCount;
         }
 
 
@@ -66,31 +61,12 @@ namespace WordleSovlerLibrary
 
         public string SuggestGuess()
         {
-            wordList = wordList.Where((word) =>
-            {
-                var hasCorrectPos = true;
-                for(int i = 0; i < knownLetters.Count && hasCorrectPos; i++)
-                {
-                    if(knownNotUsedPositions[i].Contains(word[i]))
-                    {
-                        hasCorrectPos = false;
-                    }
-                    if(knownPositionLetters[i] != null)
-                    {
-                        if (knownPositionLetters[i] != word[i])
-                        {
-                            hasCorrectPos = false;
-                        }
-                    }
- 
-                }
-                var doesntContainWrongLetters = !word.Any(letter => notUsedLetters.Contains(letter)); 
-                var containsNeededLetters = knownLetters.All(letter => knownLetters.Count == 0 || word.Contains(letter));
-                var canditate = hasCorrectPos && doesntContainWrongLetters && containsNeededLetters;
-                return canditate;
-            }).ToList();
-            RankLetters();
-            var topLetters = letterRanking.Take(5);
+            wordList = wordList.Where((word) => !word.Where((letter, i) => 
+                knownNotUsedPositions[i].Contains(word[i]) || (knownPositionLetters[i] != null && knownPositionLetters[i] != word[i])).Any()
+                && !word.Any(letter => notUsedLetters.Contains(letter)) 
+                && knownLetters.All(letter => knownLetters.Count == 0 || word.Contains(letter))
+            ).ToList();
+            var letterCount = LetterWordCount();
             var wordScore = wordList.ToDictionary(word => word, word => word.Sum(letter => word.Count(l => letter == l) == 1 ? letterCount[letter] : 0)).OrderByDescending(score => score.Value);
             return wordScore.First().Key;
         }
